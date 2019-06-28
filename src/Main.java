8b147943d18cb44472a4a338660ee77a9c53204a
jdelacruz07 <jdelacruz0077@gmail.com>
2019-06-28 12:58:31 +0200

    Fase3 en desarrollo

--- src/Main.java
+++ src/Main.java
@@ -44,23 +44,68 @@ public class Main {
 	
 	public static void showdata(ArrayList<Rocket> rockets) {
 		Rocket rocketzero = new Rocket();
+		Propulsor propulsor = new Propulsor();
 		int[] power1 = { 10, 30, 80 };
 		int[] power2 = { 30, 40, 50, 50, 30, 10 };
+//		int powerMax1 = 0;
+//		int powerMax2 = 0;
+//		
+//		for (int i = 0; i < power1.length; i++) {
+//			if (powerMax1 >= power1[i]) {
+//			} else {
+//				powerMax1 = power1[i];
+//			}
+//					
+//		}
+//		
+//		for (int i = 0; i < power2.length; i++) {
+//			if (powerMax2 >= power2[i]) {
+//			} else {
+//				powerMax2 = power2[i];
+//			}
+//			
+//		}
+//		System.out.println("potetncia maxima" + powerMax1);
+//		System.out.println("potetncia maxima" + powerMax2);
 		
 		rocketzero = rockets.get(0);
-		System.out.print(rocketzero.idRocket + " "+ rocketzero.numberPropulsor+ " ");
+		System.out.print(rocketzero.idRocket + " "+ rocketzero.numberPropulsor+  " " );
 		for (int i : power1) {
 			System.out.print(i);
 			System.out.print(" ");
 		}
+//		System.out.println(powerMax1);
 		System.out.println(" ");
 		
 		rocketzero = rockets.get(1);
-		System.out.print(rocketzero.idRocket + " "+ rocketzero.numberPropulsor+ " ");
+		System.out.print(rocketzero.idRocket + " "+ rocketzero.numberPropulsor+ "  " );
 		for (int i : power2) {
 			System.out.print(i);
 			System.out.print(" ");
 		}
+		System.out.println(" ");
+
+		for (int i : power1) {
+			System.out.println("Primer cohete acelera hasta "+ i);
+			propulsor.aceelerate(i);
+		}
+
+		for (int i : power2) {
+			System.out.println("Segundo cohete acelera hasta "+ i);
+			propulsor.aceelerate(i);
+		}
+
+		System.out.println("///////////////////////////////////////////////////////////////");
+		for (int i : power1) {
+			System.out.println("Primer cohete acelera hasta "+ i);
+			propulsor.brake(i);
+		}
+		
+		for (int i : power2) {
+			System.out.println("Segundo cohete acelera hasta "+ i);
+			propulsor.brake(i);
+		}
+
 		
 	}
 }
--- src/Propulsor.java
+++ src/Propulsor.java
@@ -1,4 +1,34 @@
 
 public class Propulsor {
-	int powerPrpulsor;
+	int powerPropulsor;
+	
+	public void aceelerate(int powerMax) {
+		int power = 0;
+		while (power >= 0 && power <= powerMax) {
+			if (power >= 0 && power <= powerMax  ){
+				power +=10;
+				System.out.println("Potencia actual " + power);
+			} else {
+				System.out.println("no more power ....... no power" + powerMax );
+			}
+		
+		}
+		System.out.println("no more power ..........." + powerMax  );
+
+	}
+	
+	public void brake(int powerNow) {
+		int power = 10;
+		while ( powerNow > 0 ) {
+			if (powerNow >= 10 ){
+				powerNow = powerNow - 10;
+				System.out.println("Potencia actual " + powerNow);
+			} else {
+				System.out.println("ha llegado a la potencia minima  ???????????????" + powerNow );
+			}
+			
+		}
+		System.out.println("potencia minima   ???????????????" + powerNow  );
+		
+	}
 }
