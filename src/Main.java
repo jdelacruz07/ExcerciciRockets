2f95b35b8c01c9a31313de80bf3cf6b35169dd26
jdelacruz07 <jdelacruz0077@gmail.com>
2019-07-06 10:43:59 +0200

    Implementando hilo a la aceleracion

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class Main {
 
-	public static void main(String[] args) {
+	static ArrayList<Integer> velocityBackup;
+	static int times;
+
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		Rocket rocket = new Rocket();
@@ -42,17 +46,35 @@ public class Main {
 
 		Propulsor propulsor = new Propulsor();
 		System.out.println("Cuantas veces quieres acelerar?");
-		int times = sc.nextInt();
+		times = sc.nextInt();
 
-		ArrayList<Integer> velocityBackup = Propulsor.timesAccelerate(power2, times);
+		(new Thread(new HelloRunnable())).start();
+		Runnable r1 = new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                //perform some work inside the thread
+                System.out.println("Hello from "+
+                        Thread.currentThread().getName()
+                        + " NOT USING LAMBDA");
+                velocityBackup = Propulsor.timesAccelerate(power1, times);
+                System.out.println("Salida --> 1 " + velocityBackup);
+            }
+        };
 
+        Thread t1 = new Thread(r1, "Thread t1");
+        t1.start();
+        
+        Thread.sleep(1000);
+        
 		System.out.println("///////                  FRENA                       ////////////////////////////");
 
+		System.out.println("Entrada " + velocityBackup);
 		System.out.println("Cuantas veces quieres desacelerar?");
 		times = sc.nextInt();
 
 		ArrayList<Integer> velocityBackup2 = new ArrayList<>();
-		System.out.println("Entrada " + velocityBackup);
 
 		for (int i = 0; i < times; i++) {
 			for (int powerMax : velocityBackup) {
